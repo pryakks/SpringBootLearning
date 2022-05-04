b1c4feb73338423aaaf1c82f2f0e5d0fce401ed5
Priya Kokas <prikokas@publicisgroupe.net>
2022-05-05 01:24:56 +0530

    Added custom Actuator

--- employeemanagementactuator/src/main/java/com/employee/management/actuator/services/DatabaseService.java
+++ employeemanagementactuator/src/main/java/com/employee/management/actuator/services/DatabaseService.java
@@ -2,7 +2,9 @@ package com.employee.management.actuator.services;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DatabaseService implements HealthIndicator {
 
     private final String DATABASE_SERVICE ="Database Service";
