100a518e5afd6254ff910b4ef4a092f62bd03320
Priya Kokas <prikokas@publicisgroupe.net>
2022-05-05 01:24:56 +0530

    added custom acruaror

--- employeemanagementactuator/src/main/java/com/employee/management/actuator/services/LoggerService.java
+++ employeemanagementactuator/src/main/java/com/employee/management/actuator/services/LoggerService.java
@@ -2,12 +2,15 @@ package com.employee.management.actuator.services;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoggerService  implements HealthIndicator {
 
     private final String LOGGER_SERVICE ="Database Service";
     @Override
     public Health health() {
+        System.out.println("checking health");
         if(isLoggerServiceHealthGood()){
             return Health.up().withDetail(LOGGER_SERVICE, "Service is running").build();
         }
